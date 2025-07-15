58dc35a2d54b731f4c72590d114e84b0f365afee
bertxoxo <johnrobertpahiculay@gmail.com>
2025-07-15 15:25:37 +0800

    Update ScreenReceiver.java

--- app/src/main/java/com/sleepmonitor/app/ScreenReceiver.java
+++ app/src/main/java/com/sleepmonitor/app/ScreenReceiver.java
@@ -7,9 +7,8 @@ import android.content.Intent;
 public class ScreenReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
-        String action = intent.getAction();
-        Intent serviceIntent = new Intent(context, SleepService.class);
-        serviceIntent.putExtra("screen_event", action);
-        context.startService(serviceIntent);
+        Intent i = new Intent(context, SleepService.class);
+        i.putExtra("event", intent.getAction());
+        context.startService(i);
     }
 }
