d85ea7163fed2466445eccef4f63dca4c95395f9
bertxoxo <johnrobertpahiculay@gmail.com>
2025-07-15 15:24:37 +0800

    Update MainActivity.java

--- app/src/main/java/com/sleepmonitor/app/MainActivity.java
+++ app/src/main/java/com/sleepmonitor/app/MainActivity.java
@@ -5,11 +5,11 @@ import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String USER_CODE = "john123";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        startService(new Intent(this, SleepService.class));
-    }
-}
+        startService(new Intent(this, S
